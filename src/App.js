f53bafd3eeeea89f1168f407225347d79c04681b
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-07 13:01:01 -0500

    Configuración del componente App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import CreateAccount from './Components/CreateAccount';
+import Login from './Components/Login';
+import Deposit from './Components/Deposit';
+import Balance from './Components/Balance';
+import AllData from './Components/AllData';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/CreateAccount/" component={CreateAccount} />
+        <Route path="/Login/" component={Login} />
+        <Route path="/Deposit/" component={Deposit} />
+        <Route path="/Balance/" component={Balance} />
+        <Route path="/AllData/" component={AllData} />
+      </Switch>
+    </Router>
   );
 }
 
