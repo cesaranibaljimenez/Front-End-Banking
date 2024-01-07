0116258c282d28c41286c8ffab756825dddd56ac
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-07 15:26:44 -0500

    Se crea la ruta para Withdraw

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Components/Home';
 import CreateAccount from './Components/CreateAccount';
 import Login from './Components/Login';
 import Deposit from './Components/Deposit';
+import Withdraw from './Components/Withdraw';
 import Balance from './Components/Balance';
 import AllData from './Components/AllData';
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/CreateAccount/" component={CreateAccount} />
         <Route path="/Login/" component={Login} />
         <Route path="/Deposit/" component={Deposit} />
+        <Route path="/Withdraw" component={Withdraw} />
         <Route path="/Balance/" component={Balance} />
         <Route path="/AllData/" component={AllData} />
       </Switch>
