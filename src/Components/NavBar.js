74f4ddf26e9f4108a5e287e4e4bc3a27cb76aaf7
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-07 17:39:53 -0500

    Se utilizan las clases de Bootstrap para darle estilo al NavBar y agregar elementos de navegación

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-function NavBar(){
-  return(
-    <>
-    <Link to="/">BadBank</Link>,
-    <Link to="/CreateAccount/">Create Account</Link>,
-    <Link to="/Login/">Login</Link>,
-    <Link to="/Deposit/">Deposit</Link>,
-    <Link to="/Withdraw/">Withdraw</Link>,
-    <Link to="/Balance/">Balance</Link>,
-    <Link to="/Alldata/">AllData</Link>
-    </>
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">BadBank</Link>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/CreateAccount/">Create Account</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Login/">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Deposit/">Deposit</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Withdraw/">Withdraw</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Balance/">Balance</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/AllData/">AllData</Link>
+          </li>
+         
+        </ul>
+      </div>
+    </nav>
   );
 }
 
