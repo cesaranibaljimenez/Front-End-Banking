0533f5f8940153b482ec380ca6c1950c93373c7c
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-10 20:04:43 -0500

    Se configura el componente AllData

--- src/Components/AllData.js
+++ src/Components/AllData.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Card, Table } from 'react-bootstrap'; // Importar desde react-bootstrap
 import { UserContext } from '../context';
 
+function AllData() {
+    const { users } = useContext(UserContext);
 
-
-
-function AllData(){
-  const ctx = React.useContext(UserContext);
-  console.log('Datos del contexto en AllData: ', ctx)
-  return (
-    <>
-    <h1>All Data<br/>
-        {JSON.stringify(ctx)}
-    </h1>
-    </>
-  );
+    return (
+        <Card className="mt-4" bg="light">
+            <Card.Body>
+                <Card.Title>All User Data</Card.Title>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Password</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user, index) => ( // Corregir la función map aquí
+                            <tr key={index}>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.password}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default AllData;
+
