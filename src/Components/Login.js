a5677572c1e779d7a2c53a366982d772c8e8c6bf
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-10 20:53:51 -0500

    Configuración del Login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Card, Form, Button, Alert } from 'react-bootstrap';
+import { UserContext } from '../context';
 
-function Login(){
-  return (
-    <h1>Login</h1>
-  )  
+function Login() {
+  const[email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const {users, setCurrentUSer} = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError('');
+  
+    //Lógica para verificar las credenciales del usurio
+  const userFound = users.find(user => user.email === email);
+  
+    if(userFound && userFound.password === password){
+        setCurrentUSer(userFound);
+        navigate('/') //Redireccionamiento del usuario al Home
+    } else {
+      setError('Credenciales incorrectas');
+    }
 }
 
+
+
+  return (
+    <Card>
+        <Card.Body>
+            <Card.Title>Login</Card.Title>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Button type="submit">Login</Button>
+            </Form>
+            {error && <Alert variant="danger">{error}</Alert>}
+        </Card.Body>
+    </Card>
+  );
+  
+};
+
 export default Login;
+
+
