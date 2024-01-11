cc701251e0ea3c79d73d213dac2532d941df09b3
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-10 19:12:46 -0500

    Se realizaron cambios en la sitaxis del estado depositAmount

--- src/Components/Deposit.js
+++ src/Components/Deposit.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { Card, Form, Button, Alert } from 'react-bootstrap';
+import { UserContext } from '../context';
 
-function Deposit(){
-  return (
-    <h1>Deposit</h1>
-  )
+function Deposit() {
+    const [depositAmount, setDepositAmount] = useState('');
+    const [statusMessage, setStatusMessage] = useState('');
+    const [showStatus, setShowStatus] = useState(false);
+    const [statusType, setStatusType] = useState(''); // 'success' o 'danger'
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+
+    const handleDeposit = (e) => {
+        e.preventDefault();
+        setShowStatus(false);
+
+        // Validar entrada numérica
+        if (isNaN(depositAmount) || depositAmount === '') {
+            setStatusMessage('Please enter a numerical value.');
+            setStatusType('danger');
+            setShowStatus(true);
+            return;
+        }
+
+        // Validar números positivos
+        const amount = parseFloat(depositAmount);
+        if (amount < 0) {
+            setStatusMessage('Only positive numbers are allowed')
+            setStatusType('danger');
+            setShowStatus(true);
+            return;
+        }
+
+        // actualizar el saldo
+        const updatedUser = {...currentUser, balance: currentUser.balance + amount };
+        setCurrentUser(updatedUser);
+        setStatusMessage('Deposit succesful.');
+        setStatusType('succes'); 
+        setShowStatus(true);   
+        
+        setDepositAmount('');
+    };
+
+    return (
+        <Card bg="primary" text="white" style={{ maxWidth: '18rem' }}>
+            <Card.Header>Deposit</Card.Header>
+            <Card.Body>
+                <Card.Title>Current Balance: ${currentUser?.balance}</Card.Title>
+                <Form onSubmit={handleDeposit}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Amount</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={depositAmount}
+                            onChange={(e) => setDepositAmount(e.target.value)}
+                            placeholder="Enter amount"
+                        />
+                    </Form.Group>
+                    <Button variant="light" type="submit" disabled={depositAmount === ''}>
+                        Deposit
+                    </Button>
+                </Form>
+                {showStatus && <Alert variant={statusType}>{statusMessage}</Alert>}
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default Deposit;
