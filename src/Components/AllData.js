f3a2739ca3e8fb050074038290a5b39d27896116
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-11 12:52:52 -0500

    Se corrige la sintaxis del código de la tabla de acuerdo a la V5 de bootstrap

--- src/Components/AllData.js
+++ src/Components/AllData.js
@@ -5,32 +5,37 @@ import { UserContext } from '../context';
 function AllData() {
     const { users } = useContext(UserContext);
 
+
     return (
-        <Card className="mt-4" bg="light">
-            <Card.Body>
-                <Card.Title>All User Data</Card.Title>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Password</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map((user, index) => ( // Corregir la función map aquí
-                            <tr key={index}>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.password}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </Card.Body>
-        </Card>
-    );
+      <Card className="mt-4 mx-3" bg="light">
+          <Card.Body>
+              <Card.Title>All User Data</Card.Title>
+              <Table striped bordered hover>
+                  <thead>
+                      <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Name</th>
+                          <th scope="col">Email</th>
+                          <th scope="col">Password</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {users.map((user, index) => (
+                          <tr key={index}>
+                              <th scope="row">{index + 1}</th>
+                              <td>{user.name}</td>
+                              <td>{user.email}</td>
+                              <td>{user.password}</td>
+                          </tr>
+                      ))}
+                  </tbody>
+              </Table>
+          </Card.Body>
+      </Card>
+  );
 }
+
+
 
 export default AllData;
 
