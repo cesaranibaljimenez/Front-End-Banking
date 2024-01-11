d8e00b28ddacff8fb299a28f74d15a49977fe036
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-11 18:09:55 -0500

    Se agregan descripciones de cada página para que se viaulicen al pasar el curso sobre los nav-link

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,19 +8,19 @@ function NavBar() {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link className="nav-link" to="/CreateAccount/">Create Account</Link>
+            <Link className="nav-link" to="/CreateAccount/" title="Crear Cuenta">Create Account</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/Login/">Login</Link>
+            <Link className="nav-link" to="/Login/" title="Iniciar Sesión">Login</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/Deposit/">Deposit</Link>
+            <Link className="nav-link" to="/Deposit/" title="Realizar un depósito">Deposit</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/Withdraw/">Withdraw</Link>
+            <Link className="nav-link" to="/Withdraw/" title="Realizar un retiro">Withdraw</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/AllData/">AllData</Link>
+            <Link className="nav-link" to="/AllData/" title="Ver cuentas">AllData</Link>
           </li>
          
         </ul>
