f4da64c0e1b1c0c7cf14dbadfd18c5e3a1cbab19
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-12 12:53:24 -0500

    Se actualiza el contexto para poder realizar seguimiento de las transacciones

--- src/context.js
+++ src/context.js
@@ -3,11 +3,24 @@ import React, { useState, createContext } from 'react';
 export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
 const [users, setUsers] = useState([]);
-const [currentUser, setCurrentUser] = useState({ balance: 0 });
+const [currentUser, setCurrentUser] = useState({ balance: 0 , transaction: [] });
+
+//Función para agregar una nueva transacción
+const addTransaction = (email, type, amount) => {
+  setUsers(users.map(user => {
+    if(user.email === email) {
+      return {
+          ...user,
+          transactions: [...user.transaction, {type, amount}]
+      };
+    }
+    return user;
+  }));
+};
 
 console.log("Usuarios al inicializar:", users);//depuración
   return(
-    <UserContext.Provider value={{ users, setUsers, currentUser, setCurrentUser}}>
+    <UserContext.Provider value={{ users, setUsers, currentUser, setCurrentUser, addTransaction}}>
         {children}
     </UserContext.Provider>
 
