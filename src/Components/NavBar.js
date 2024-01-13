c34925e611662865bea5805de2cc29d58dcf9fff
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-13 18:48:14 -0500

    Se agrega la className navbar-toogler para un diseño responsivo del NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,6 +5,9 @@ function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">BadBank</Link>
+      <button className="navbar-toogler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
