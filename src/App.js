8be7c5dae20744c5ef20c62573829004bfa5f484
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-13 19:10:08 -0500

    Se realiza la importación bootstrap js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { UserProvider } from './context';
 import NavBar from './Components/NavBar';
 import Home from './Components/Home';
