76693c07eb28c8262c9087eaf022859c4e31b9e3
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2024-01-13 19:09:10 -0500

    Se añade al ClassName navar-toogler d-lg-none para no presentar el botón en pantallas grandes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">BadBank</Link>
-      <button className="navbar-toogler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toogler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
